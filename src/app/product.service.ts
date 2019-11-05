343755e342ce4d935a77c7bb7c35b1aec707709e
Irene0000 <agbugbaijeoma@gmail.com>
2019-11-05 10:38:56 +0100

    module 4 complete

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,10 +9,9 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) {  }
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => 
-    response.json());
-    return new Observable<Album>()
+    <Album>response.json());
   }
 
 }
